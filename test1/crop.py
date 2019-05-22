3fa8268c1e541f7f479808f9c06df4c6e6a07879
i-an-chen <50688645+i-an-chen@users.noreply.github.com>
2019-05-22 15:34:20 +0800

    Update crop.py

--- test1/crop.py
+++ test1/crop.py
@@ -9,19 +9,19 @@ def IMAGE_CROP():
     train = sio.loadmat(train_mat)['annotations']
     test = sio.loadmat(test_mat)['annotations']
     for i in range(len(train[0])):
-            train_image = Image.open("./cars_train/"+train[0][i][5][0])
+            train_image = Image.open("~/cars_train/"+train[0][i][5][0])
             train_image=train_image.crop((train[0][i][0][0][0], train[0][i][1][0][0], train[0][i][2][0][0], train[0][i][3][0][0]))
-            directory = os.path.dirname("./cars_train_crop/"+str(train[0][i][4][0][0])+"/")
+            directory = os.path.dirname("~/cars_train_crop/"+str(train[0][i][4][0][0])+"/")
             if not os.path.exists(directory):
                 os.makedirs(directory)
-            train_image.save("./cars_train_crop/"+str(train[0][i][4][0][0])+'/'+train[0][i][5][0])
+            train_image.save("~/cars_train_crop/"+str(train[0][i][4][0][0])+'/'+train[0][i][5][0])
     for i in range(len(test[0])):
-            test_image = Image.open("./cars_test/"+test[0][i][5][0])
+            test_image = Image.open("~/cars_test/"+test[0][i][5][0])
             test_image=test_image.crop((test[0][i][0][0][0], test[0][i][1][0][0], test[0][i][2][0][0], test[0][i][3][0][0]))
-            directory = os.path.dirname("./cars_test_crop/"+str(test[0][i][4][0][0])+"/")
+            directory = os.path.dirname("~/cars_test_crop/"+str(test[0][i][4][0][0])+"/")
             if not os.path.exists(directory):
                 os.makedirs(directory)
-            test_image.save("./cars_test_crop/"+str(test[0][i][4][0][0])+'/'+test[0][i][5][0])
+            test_image.save("~/cars_test_crop/"+str(test[0][i][4][0][0])+'/'+test[0][i][5][0])
 
 if __name__ == '__main__':
 	IMAGE_CROP()
