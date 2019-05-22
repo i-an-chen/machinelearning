8a6fe6ceadf9587ffcc2eb5fdba1e5efc6e8e1ef
i-an-chen <50688645+i-an-chen@users.noreply.github.com>
2019-05-22 16:14:22 +0800

    Update dataset.py

--- test1/dataset.py
+++ test1/dataset.py
@@ -13,13 +13,13 @@ class IMAGE_Dataset(Dataset):
         self.data = sio.loadmat(matfn)['annotations']
         self.transform = transform
         for i in range(len(self.data[0])):
-                self.x.append(str(self.root_dir)+"/"+str(self.data[0][i][4][0][0])+"/"+str(self.data[0][i][5][0]))
+                self.x.append(../../str(self.root_dir)+"/"+str(self.data[0][i][4][0][0])+"/"+str(self.data[0][i][5][0]))
                 self.y.append(int(self.data[0][i][4][0][0]))
     def __len__(self):
         return len(self.x)
 
     def __getitem__(self, index):
-        image = Image.open('./'+str(self.root_dir)+"/"+str(self.data[0][index][4][0][0])+"/"+str(self.data[0][index][5][0])).convert('RGB')
+        image = Image.open('../../'+str(self.root_dir)+"/"+str(self.data[0][index][4][0][0])+"/"+str(self.data[0][index][5][0])).convert('RGB')
         if self.transform:
             image = self.transform(image)
         return image, int(self.data[0][index][4][0][0])
